9f24c7eb1e2df9a3867e0c5d680e9ff1d1a92751
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-09-24 02:52:37 -0500

    second entrega bby

--- Fixed-Route/Main.py
+++ Fixed-Route/Main.py
@@ -1,15 +1,28 @@
 from Data import Data
 from Vehicle import Vehicle
 from Methods import *
+import time
+from matplotlib.cbook import tostr
 
 
 def main():
     v = Vehicle()
+    v2 = Vehicle()
     d = Data()
     d.saveRoute()
-#     d.generateNoise()
-    ConstructiveAlgorithm(d, v, 1, 10)
-#     d.printDataFromFile()
-    print("test")
+    
+    #first method
+    start = time.time()
+    ConstructiveAlgorithm(d, v, 1, 30)
+    end = time.time()
+    print("elapsed time " + str( end - start))
+    print()
+    
+    #second method
+    start = time.time()
+    NoiseAndConstruc(d, v2, 1, 30)
+    end = time.time()
+    print("elapsed time " + str( end - start))
+
     
 if __name__ == "__main__": main()
--- Fixed-Route/Methods.py
+++ Fixed-Route/Methods.py
@@ -1,8 +1,21 @@
-def canIArrive(vehicle,currentNode, nextNode):
+def checkConditions(vehicle,currentNode, nextNode):
     tempVehicle = vehicle
-    milesTotravel = nextNode.cumMile - currentNode.cumMile
-    currentFuel_gal = tempVehicle.currentFuel - ((milesTotravel**-1) *  tempVehicle.milesPerGallon )**-1 # converting unities
-    print("hola")
+    milesTotravel = nextNode.cumMile - currentNode.cumMile 
+    if milesTotravel != 0.0:
+        currentFuel_gal = tempVehicle.currentFuel - ((milesTotravel**-1) *  tempVehicle.milesPerGallon )**-1 # converting unities
+        return currentFuel_gal >= tempVehicle.minfuelAnyTime_Gall
+    return True
+
+def advance(vehicle,currentNode, nextNode):
+    if currentNode.cumMile != nextNode.cumMile: # have to think well this question
+        milesTotravel = nextNode.cumMile - currentNode.cumMile
+        vehicle.currentFuel = vehicle.currentFuel - (((milesTotravel**-1) *  vehicle.milesPerGallon )**-1)
+        
+def AsapFuelPolicy(vehicle,currentNode):
+    toRefuel = vehicle.tankCapacity_Gall - vehicle.currentFuel
+    price = toRefuel * currentNode.price
+    vehicle.currentFuel = vehicle.currentFuel +  toRefuel
+    vehicle.acumulatePrice = vehicle.acumulatePrice + price  
     
 
 def ConstructiveAlgorithm(data,vehicle, idStart, idEnd):
@@ -10,7 +23,24 @@ def ConstructiveAlgorithm(data,vehicle, idStart, idEnd):
     arrive = False
     while current != idEnd:
         currentNode = data.route[current]
-        nextNode = data.route[current + 1]   
+        nextNode = data.route[current + 1]
         current =current + 1
-        canIArrive(vehicle, currentNode, nextNode)
+        if(checkConditions(vehicle, currentNode, nextNode)):
+            advance(vehicle, currentNode, nextNode)
+        else:
+            AsapFuelPolicy(vehicle, currentNode)
+            if(checkConditions(vehicle, currentNode, nextNode)):
+                advance(vehicle, currentNode, nextNode)
+            else:
+                print("No Solution know :( sorry")
+    print("constructive algorithm")
+    print (vehicle.acumulatePrice)
+    
+def NoiseAndConstruc(data,vehicle, idStart, idEnd):
+    NoisedData = data
+    NoisedData.generateNoise()
+    print("Noised Algorithm")
+    ConstructiveAlgorithm(NoisedData,vehicle, idStart, idEnd)
+    
+
     
