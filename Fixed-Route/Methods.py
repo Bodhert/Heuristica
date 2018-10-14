302b53c65d04bb2b6246d24ae113057fbb5b2749
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-10-13 21:48:10 -0500

    neighborhood created. TODO refactoring

--- Fixed-Route/Methods.py
+++ Fixed-Route/Methods.py
@@ -1,3 +1,5 @@
+# refueling_nodes =[]
+
 def checkConditions(vehicle,currentNode, nextNode):
     tempVehicle = vehicle
     milesTotravel = nextNode.cumMile - currentNode.cumMile 
@@ -11,6 +13,7 @@ def advance(vehicle,currentNode, nextNode):
         milesTotravel = nextNode.cumMile - currentNode.cumMile
         vehicle.currentFuel = vehicle.currentFuel - (((milesTotravel**-1) *  vehicle.milesPerGallon )**-1)
         
+        
 def AsapFuelPolicy(vehicle,currentNode):
     toRefuel = vehicle.tankCapacity_Gall - vehicle.currentFuel
     price = toRefuel * currentNode.price
@@ -21,6 +24,7 @@ def AsapFuelPolicy(vehicle,currentNode):
 def ConstructiveAlgorithm(data,vehicle, idStart, idEnd):
     current = idStart
     arrive = False
+    refueling_nodes = []
     while current != idEnd:
         currentNode = data.route[current]
         nextNode = data.route[current + 1]
@@ -30,17 +34,37 @@ def ConstructiveAlgorithm(data,vehicle, idStart, idEnd):
         else:
             AsapFuelPolicy(vehicle, currentNode)
             if(checkConditions(vehicle, currentNode, nextNode)):
+                refueling_nodes.append(currentNode)
                 advance(vehicle, currentNode, nextNode)
             else:
                 print("No Solution know :( sorry")
     print("constructive algorithm")
     print (vehicle.acumulatePrice)
+    return refueling_nodes
+
+
+
     
 def NoiseAndConstruc(data,vehicle, idStart, idEnd):
     NoisedData = data
     NoisedData.generateNoise()
     print("Noised Algorithm")
     ConstructiveAlgorithm(NoisedData,vehicle, idStart, idEnd)
-    
+
+def nodes_stop(data,vehicle,idStart,idEnd):
+    return  ConstructiveAlgorithm(data, vehicle, idStart, idEnd)
+
+def make_move_on_a_neighborhood(initial_neighborhood, data):
+    if len(initial_neighborhood) != 0:
+        next_neighborhood = []
+        for node in initial_neighborhood:
+            alteration = data.route[node.stopId + 5]
+            next_neighborhood.append(alteration)
+        return next_neighborhood
+    else:
+        print("sorry no empty list allowed")
+            
+        
+
 
     
